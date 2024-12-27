7221aab47336e66a9008172a5c6faf0d3f11d676
Vladyslav <prostoandrei9@gmail.com>
2024-12-27 21:47:02 +0200

    fix: scrollbar location

--- src/components/widgets/Chat.tsx
+++ src/components/widgets/Chat.tsx
@@ -4,7 +4,7 @@ import { MessageForm, MessageList } from "@/components";
 
 export const Chat: FC = () => {
   return (
-    <div className="w-full h-[calc(100vh-73px)] flex flex-col px-8 py-4 items-center overflow-hidden">
+    <div className="w-full h-[calc(100vh-73px)] flex flex-col px-4 py-4 items-center overflow-hidden">
       <MessageList />
       <MessageForm />
     </div>
