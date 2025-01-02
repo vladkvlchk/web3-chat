6a53fa685cb62ec9b2db27a3a1fffcd83f378b1c
Vladyslav <prostoandrei9@gmail.com>
2025-01-02 15:11:54 +0200

    fix: view-on-explorer button margin

--- src/components/ui/ViewOnExplorerButton.tsx
+++ src/components/ui/ViewOnExplorerButton.tsx
@@ -1,10 +1,9 @@
 import { ToastAction } from "@radix-ui/react-toast";
 import { ExternalLink } from "lucide-react";
-import { Separator } from "./separator";
 
 export function ViewOnExplorerButton({ hash }: { hash: string }) {
   return (
-    <ToastAction altText={"view on explorer"} className="border px-3 py-2 rounded-md">
+    <ToastAction altText={"view on explorer"} className="border px-3 py-2 rounded-md ml-auto">
       <a
         href={`https://sepolia.etherscan.io/tx/${hash}`}
         target="_blank"
